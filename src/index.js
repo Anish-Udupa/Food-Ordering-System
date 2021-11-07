88f77e57c0834448763ed134a584e9526116ce57
Anish-Udupa <udupa.anish@gmail.com>
2021-11-07 13:59:58 +0530

    Developing MenuItem component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MenuItem from './menu-page/MenuItem';
+import pizza from './pizza.jpg';
+
+const prop = {
+  title: "Pizza",
+  desc: "This is the description of a pizza",
+  imgsrc: {pizza},
+  imgalt: "Image of a pizza"
+};
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MenuItem {...prop}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/menu-page/MenuItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+{
+    /*
+    Props:
+        -> title
+        -> desc
+
+    */
+}
+
+class MenuItem extends React.Component{
+    render(){
+        
+    console.log(this.props.imgsrc.pizza);
+        return (
+            <div className="menu-item">
+                <img src={this.props.imgsrc.pizza} alt={this.props.imgalt} />
+                <p className="title">{this.props.title}</p>
+                <p className="desc">{this.props.desc}</p>
+
+            </div>
+        );
+    }
+}
+
+export default MenuItem;
