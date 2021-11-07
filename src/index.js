fe8534891af59a9758ee59d1d98fb473b89d0029
Anish-Udupa <udupa.anish@gmail.com>
2021-11-07 15:13:00 +0530

    updated comments in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    // Insert your code below this
+    {/* Insert your code below this */}
 
-    // Insert your code above this
+    {/* Insert your code above this */}
   </React.StrictMode>,
   document.getElementById('root')
 );
