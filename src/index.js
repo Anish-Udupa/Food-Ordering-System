36d0958b5cfd2f3abc9f7c649e1bd2e034f2c017
Anish-Udupa <udupa.anish@gmail.com>
2021-11-18 20:56:45 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MenuPage from './menu-page/MenuPage';
-//import MenuItem from './menu-page/MenuItem';
-//import pizza from './pizza.jpg';
-
-// const prop = {
-//   title: "Classic Margerita",
-//   desc: "This is the description of a pizza",
-//   imgsrc: {pizza},
-//   imgalt: "Image of a pizza",
-//   price: 2000
-// };
 
 ReactDOM.render(
   <React.StrictMode>
