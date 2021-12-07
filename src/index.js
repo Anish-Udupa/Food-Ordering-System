a815689049f948edd56cfc7d09ef68d59522a202
Adarsh Puranik <puranikadarshk@gmail.com>
2021-12-07 23:22:09 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MenuPage from './menu-page/MenuPage';
+import FeedbackForm from './feedback-form/FeedbackForm';
 
+import LoginPage from './login-page/LoginPage'
 ReactDOM.render(
   <React.StrictMode>
     {/* Insert your code below this */}
     <MenuPage />
+    <FeedbackForm/>
+    <LoginPage/>
     {/* Insert your code above this */}
   </React.StrictMode>,
   document.getElementById('root')
