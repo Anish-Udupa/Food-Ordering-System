478bd21f2baad898a2b5369f6b533a3a4823a1ba
Anish-Udupa <udupa.anish@gmail.com>
2022-01-19 17:38:21 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import MenuPage from './menu-page/MenuPage';
+import ContactPage from './contact-page/ContactPage'
+import OrderSuccessful from './order-confirm-page/OrderConfirmPage'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,8 @@ ReactDOM.render(
       <Routes>
         <Route path='/' element={<App />} />
         <Route path='/menu' element={<MenuPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/order-successful' element={<OrderSuccessful />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
