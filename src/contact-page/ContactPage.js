19ff739e2858d76aec6270589c0acf9659d917c7
Anish-Udupa <udupa.anish@gmail.com>
2022-01-19 17:38:44 +0530

    Created a contact page

---
+++ src/contact-page/ContactPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Header from '../header/Header';
+import Form from './Form';
+
+class ContactPage extends Component {
+    render() {
+      return (
+          <>
+            <Header />
+            <Form />
+          </>
+      );
+    }
+  }
+
+  
+  export default ContactPage;
