3e6784ba5aa313f1cc347ec2ed2123be9ba880e1
Anish-Udupa <udupa.anish@gmail.com>
2022-10-25 16:56:50 +0530

    Improved order details

--- src/order-confirm-page/components/order-details/OrderDetails.js
+++ src/order-confirm-page/components/order-details/OrderDetails.js
@@ -7,15 +7,24 @@ const OrderDetails = () => {
         {item_name: "Pasta", item_quantity: 4, item_price: 50000},
         {item_name: "Pepsi", item_quantity: 1, item_price: 50},
         {item_name: "Pepsi", item_quantity: 1, item_price: 50},
-        {item_name: "Pepsi", item_quantity: 1, item_price: 50},
-        {item_name: "Pepsi", item_quantity: 1, item_price: 50},
-        {item_name: "Pepsi", item_quantity: 1, item_price: 50},
-        {item_name: "Pepsi", item_quantity: 1, item_price: 50},
-        {item_name: "Pepsi", item_quantity: 1, item_price: 50},
-        {item_name: "Pepsi", item_quantity: 1, item_price: 50},
+        // {item_name: "Pepsi", item_quantity: 1, item_price: 50},
+        // {item_name: "Pepsi", item_quantity: 1, item_price: 50},
+        // {item_name: "Pepsi", item_quantity: 1, item_price: 50},
+        // {item_name: "Pepsi", item_quantity: 1, item_price: 50},
+        // {item_name: "Pepsi", item_quantity: 1, item_price: 50},
+        // {item_name: "Pepsi", item_quantity: 1, item_price: 50},
     ]
     return (
-        <ListItemContainer items={sample} />
+        <div id="order-details-container">
+            <p>Order Details</p>
+            <div id="order-details-list-item-container">
+                <ListItemContainer items={sample} />
+            </div>
+            <div id="order-details-total-cost-container">
+                <p>Total cost</p>
+                <p>Rs. 1233</p>
+            </div>
+        </div>
     )
 }
 
