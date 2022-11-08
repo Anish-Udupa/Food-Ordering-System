89a7902e89c43ab6a3810f8faf2e6471ddeed024
Anish Udupa <udupa.anish@gmail.com>
2022-11-08 22:06:27 +0530

    Updated Order confirm page

--- src/order-confirm-page/OrderConfirmPage.js
+++ src/order-confirm-page/OrderConfirmPage.js
@@ -2,17 +2,32 @@ import "./OrderConfirmPage.css";
 import OrderMap from "./components/order-map/OrderMap";
 import OrderStage from "./components/order-stage/OrderStage";
 import OrderDetails from "./components/order-details/OrderDetails";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function OrderConfirmPage(props){
+    const navigate = useNavigate();
+    const [ itemsPresent, setItemPresent ] = useState(false);
+
+    // Initially check if any items are selected. If not, redirect to menu
+    useEffect(() => {
+        const items = localStorage.getItem("redux-store");
+        if(items === undefined || items === null){
+            navigate("/menu");
+        }
+        else
+            setItemPresent(true);
+    }, [])
     return (
-        <div id="order-confirm-container">
+        itemsPresent &&
+        (<div id="order-confirm-container">
             <OrderMap />
             
             <div id="order-confirm-area-container">
                 <OrderStage />
                 <OrderDetails />
             </div>
-        </div>
+        </div>)
     );
 }
 
--- src/order-confirm-page/components/order-stage/OrderStage.js
+++ src/order-confirm-page/components/order-stage/OrderStage.js
@@ -1,6 +1,7 @@
 const OrderStage = () => {
     return (
-        <p>Order Stage</p>
+        // <p>Order Stage</p>
+        <p>Your order has be placed successfully!</p>
     )
 }
 
